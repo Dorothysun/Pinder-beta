76448bba87eef9a55bdb3f66e24bb3e45e7f3f7c
NikunjAgrawal <agrawaln@grinnell.edu>
2018-01-14 15:43:06 -0500

    added data to company results

--- client/CompanyResults.jsx
+++ client/CompanyResults.jsx
@@ -1,49 +1,38 @@
 import React from 'react';
-
+var star = <span className="fa fa-star checked"></span>;
 const CompanyResults = (props) => {
 
 	return (
-		<div className="container">
-			<div className="navbar-brand">
-				<a className="navbar-item title" href="../">
-					<img id="icon" src="../img/icon.png" alt="Logo" />
-				</a>
-			</div>
-		    {/* <div class="hero-body" id="header">
-    		</div>	 */}
-			<div className="columns content-wrapper">
-			<div className="column is-4">
-
-				<div className="card">
-				<div className="card-content">
-				</div>
-				<footer className="card-footer">
-					<a className="card-footer-item yes">Interested</a>
-					<a className="card-footer-item no">Not Interested</a>
-				</footer>
-				</div>
-
-				<div className="card">
-				<div className="card-content">
-				</div>
-				<footer className="card-footer">
-					<a className="card-footer-item yes">Interested</a>
-					<a className="card-footer-item no">Not Interested</a>
-				</footer>
-				</div>
-			</div>
+		<div className="container content-wrapper">
+		    <div class="hero-body" id="header">
+    		</div>	
+			<div className="columns">
 
 			<div className="column is-4">
+
 				<div className="card">
 				<div className="card-content">
+				<strong>Experience:</strong> 2 years<br />
+				<strong>Skills:</strong> SQLi, XSS<br />
+				<strong>Certifications:</strong> CISSP, CEH<br />
+				<strong>Writing sample:</strong> <a>writingsample.doc</a><br />
+				<strong>Rating:</strong>  {star}{star}{star}<br />
+				<strong>Reviews:</strong> No reviews
 				</div>
 				<footer className="card-footer">
 					<a className="card-footer-item yes">Interested</a>
 					<a className="card-footer-item no">Not Interested</a>
 				</footer>
 				</div>
+
 				<div className="card">
 				<div className="card-content">
+				<strong>Experience:</strong> 6 years<br />
+				<strong>Skills:</strong> LAN/WAN VPN<br />
+				<strong>Certifications:</strong> CISM<br />
+				<strong>Writing sample:</strong> <a>writingsample.doc</a><br />
+				<strong>Rating:</strong>  {star}{star}{star}{star}<br />
+				<strong>Reviews:</strong> No reviews
 				</div>
 				<footer className="card-footer">
 					<a className="card-footer-item yes">Interested</a>
@@ -52,13 +41,47 @@ const CompanyResults = (props) => {
 				</div>
 			</div>
 
+			<div className="column is-4">
+				<div className="card">
+				<div className="card-content">
+				<strong>Experience:</strong> 3 years<br />
+				<strong>Skills:</strong> SSH Firewall Understanding<br />
+				<strong>Certifications:</strong> Security+<br />
+				<strong>Writing sample:</strong> <a>writingsample.doc</a><br />
+				<strong>Rating:</strong> {star}{star}{star}<br />
+				<strong>Reviews:</strong> No reviews
+				</div>
+				<footer className="card-footer">
+					<a className="card-footer-item yes">Interested</a>
+					<a className="card-footer-item no">Not Interested</a>
+				</footer>
+				</div>
+				<div className="card">
+				<div className="card-content">
+				<strong>Experience:</strong> 5 years<br />
+				<strong>Skills:</strong> Security Monitoring, Web Security Auditing<br />
+				<strong>Certifications:</strong> GSEC <br />
+				<strong>Writing sample:</strong> <a>writingsample.doc</a><br />
+				<strong>Rating:</strong>  {star}{star}<br />
+				<strong>Reviews:</strong> No reviews
+				</div>
+				<footer className="card-footer">
+					<a className="card-footer-item yes">Interested</a>
+					<a className="card-footer-item no">Not Interested</a>
+				</footer>
+				</div>
+			</div>
+
 
 			<div className="column is-4">
 				<div className="card">
 				<div className="card-content">
-					{/* <figure className="image">
-					<img src="images/cover.png" alt="Cover template screenshot">
-					</figure> */}
+				<strong>Experience:</strong> 2 years<br />
+				<strong>Skills:</strong> Virtualization Environment, Web Security Auditing<br />
+				<strong>Certifications:</strong> SSCP <br />
+				<strong>Writing sample:</strong> <a>writingsample.doc</a><br />
+				<strong>Rating:</strong>  {star}{star}{star}{star}<br />
+				<strong>Reviews:</strong> No reviews
 				</div>
 				<footer className="card-footer">
 					<a className="card-footer-item yes">Interested</a>
@@ -68,9 +91,12 @@ const CompanyResults = (props) => {
 
 				<div className="card">
 				<div className="card-content">
-					{/* <figure className="image">
-					<img src="images/inbox.png" alt="Inbox template screenshot" />
-					</figure> */}
+				<strong>Experience:</strong> 11 years<br />
+				<strong>Skills:</strong>  SQLi, Web Security Auditing<br />
+				<strong>Certifications:</strong> CISSP <br />
+				<strong>Writing sample:</strong> <a>writingsample.doc</a><br />
+				<strong>Rating:</strong>  {star}{star}{star}{star}{star}<br />
+				<strong>Reviews:</strong> No reviews
 				</div>
 				<footer className="card-footer">
 					<a className="card-footer-item yes">Interested</a>
